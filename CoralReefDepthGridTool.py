efa2ee45d05c2c0472e33cfe98f11938034f8364
NiyamITCLindeman <clindeman@niyamit.com>
2021-01-19 16:39:27 -1000

    Added user defined unit output and conversion
    Added suffix to output files to denote units, this is set by the user. Added parameter for foot to meter conversion. Added ability for user to change the output units. Removed some print statements that were used in debugging early.  Added a total runtime report.

--- CoralReefDepthGridTool.py
+++ CoralReefDepthGridTool.py
@@ -15,6 +15,8 @@ Known issues with the data circa 20201203:
 print("Start")
 
 #IMPORTS
+import time
+startTime = time.time()
 import arcpy
 from arcpy.sa import *
 import os
@@ -22,16 +24,15 @@ import os
 
 #SETTINGS
 arcpy.CheckOutExtension("Spatial")
-arcpy.env.scratchWorkspace = r"in_memory"
-arcpy.env.workspace = r"C:\temp\ScratchCoral" #set to where you want the output depthgrids (.tif) to be saved at
+arcpy.env.scratchWorkspace = "in_memory"
+arcpy.env.workspace  = r"C:\temp\ScratchCoral" #set to where you want the output depthgrids (.tif) to be saved at
 arcpy.env.overwriteOutput = True
-
+meterToUSSurveyFoot = 0.3048
 
 #FUNCTIONS
 def unique_values(table, field):  ##uses list comprehension
     with arcpy.da.SearchCursor(table, [field]) as cursor:
         return sorted({row[0] for row in cursor})
-
     
 #INPUTS
 #Input for arctoolbox operation
@@ -45,6 +46,9 @@ def unique_values(table, field):  ##uses list comprehension
 Folder = r"C:\Users\Clindeman\OneDrive - niyamit.com\CoralReef\Data\USGS" #change this to your directory
 inFloodPointsScenarioField = 'Scenario' #update this if data doesn't comform, or update data
 inFloodMaskScenarioField = 'MaskName' #update this if data doesn't comform, or update data
+zField = "F_Depth" #Should check that this exists/matches first. Used in IDW.
+inUnits = 'ft' #update this to reflect input data zField, feet or meters: 'ft' or 'm'
+outUnits = 'ft' #update this to desired output, feet or meters: 'ft' or 'm'
 
 #Locations
 '''This assumes that the folders from the source follow this format: HawaiianIslands_floodmasks
@@ -63,60 +67,90 @@ for location in LocationList:
     print(location)
     inFloodPoints = os.path.join(Folder, r'{loc}_floodpoints\{loc}_floodpoints.shp'.format(loc=location))
     inFloodMask = os.path.join(Folder, r'{loc}_floodmasks\{loc}_floodmasks.shp'.format(loc=location))
+    
     #generate a list of scenarios...
     scenariosPoints = unique_values(inFloodPoints,inFloodPointsScenarioField)
     scenariosMask = unique_values(inFloodMask,inFloodMaskScenarioField)
+    
     #Check if scenarios match and if they do create an IDW raster...
     mergedList = tuple(zip(sorted(scenariosPoints), sorted(scenariosMask)))
+
+    #Create a copy of the data so that adding a field doesnt add to the original data...
+    print("Copying point data into memory...")
+    tempInFloodPoints = "in_memory/inFloodPoints"
+    arcpy.CopyFeatures_management(inFloodPoints, tempInFloodPoints)
+
+    print("Check if user wants different output units...")
+    if inUnits != outUnits:
+        if outUnits == 'ft':
+            print('Meters to Feet...')
+            expression = '!'+zField+'! / '+str(meterToUSSurveyFoot)
+            arcpy.CalculateField_management(tempInFloodPoints, zField, expression, "PYTHON")
+            
+        elif outUnits == 'm':
+            print('Feet to Meters...')
+            expression = '!'+zField+'! * '+str(meterToUSSurveyFoot)
+            arcpy.CalculateField_management(tempInFloodPoints, zField, expression, "PYTHON")
+            
+        else:
+            print('something went wrong with the units')
+    else:
+        print(inUnits,'to',outUnits)
+    
     for x in mergedList:
-            y = x[0].split("_")#check if return period is in ReturnPeriodList
+            y = x[0].split("_") #check if return period is in ReturnPeriodList
             if y[1] in ReturnPeriodList:
                 x0 = x[0].replace("_floodpoints","")
                 x1 = x[1].replace("_floodmask","")
-                '''Maui_rp100_wrf_floodmask != Maui_rp100_wrf_floodpoints
-                   Maui_rp100_wrf == Maui_rp100_wrf'''
                 
                 if x0 <> x1:
+                    'Maui_rp100_wrf_floodmask != Maui_rp100_wrf_floodpoints'
                     print("NO Match",x0,x1)
                     
                 elif x0 == x1:
+                    'Maui_rp100_wrf == Maui_rp100_wrf'
                     print("Match",x0,x1)
+                    
                     PointsLayer = x0 + "PLayer"
                     MaskLayer = x1 + "MLayer"
-                    IDWGrid = x0 + ".tif" #grid format is failing, error 010240
-                    
-                    #iterate over each scenario, filtering the data and generating a depth grid with IDW...
+                    outputIDWGrid = x0 + '_' + outUnits + ".tif"
+
+                    #iterate over each scenario, filtering the data and generating a depth grid with IDW...                    
                     print("Make PointsLayer...", x[0], PointsLayer)
                     inFloodPointsWhereClause = '"' + inFloodPointsScenarioField + '"' + "= '" + x[0] + "'"
-                    print(inFloodPointsWhereClause)
-                    arcpy.MakeFeatureLayer_management(inFloodPoints, PointsLayer, inFloodPointsWhereClause)
+                    arcpy.MakeFeatureLayer_management(tempInFloodPoints, PointsLayer, inFloodPointsWhereClause)
 
                     print("Make MaskLayer...", x[1], MaskLayer)
                     inFloodMaskWhereClause = '"' + inFloodMaskScenarioField + '"' + "= '" + x[1] + "'"
-                    print(inFloodMaskWhereClause)
-                    arcpy.MakeFeatureLayer_management(inFloodMask, MaskLayer, inFloodMaskWhereClause)    
+                    arcpy.MakeFeatureLayer_management(inFloodMask, MaskLayer, inFloodMaskWhereClause)
 
                     try:
                         arcpy.env.mask = MaskLayer
-                        arcpy.env.cellSize = 30 
-                        zField = "F_Depth" #Sould check that this exists/matches first
-                        print("IDW", IDWGrid)
-                        outIDW = Idw(PointsLayer, zField)
+                        arcpy.env.cellSize = 30
+                        
+                        print("IDW", PointsLayer, '...')
+                        try:
+                            outIDW = Idw(PointsLayer, zField)
+                        except Exception as (e):
+                            print(e)
 
-                        print('Clipping to MaskLayer and saving...')
+                        print('Clipping to ',MaskLayer,' and saving', outputIDWGrid, '...')
                         rectangle = "{} {} {} {}".format(outIDW.extent.XMin, outIDW.extent.YMin, outIDW.extent.XMax, outIDW.extent.YMax)
                         try:
-                            arcpy.Clip_management(outIDW, rectangle, IDWGrid, MaskLayer, 0, 'ClippingGeometry', 'MAINTAIN_EXTENT')
+                            arcpy.Clip_management(outIDW, rectangle, outputIDWGrid, MaskLayer, 0, 'ClippingGeometry', 'MAINTAIN_EXTENT')
                         except Exception as (e):
                             print(e)
                             
                     except Exception as (e):
                         print(e)
+                    del rectangle
+                    arcpy.management.Delete(outIDW)
                     arcpy.management.Delete(PointsLayer)
                     arcpy.management.Delete(MaskLayer)
                     
                 else:
                     pass
                 print("")
-
-print("Done")
+                
+endTime = time.time()-startTime
+print("Done", endTime, 'seconds')
